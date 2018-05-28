d5264b33f53cf395e5f625e8ae65a72998b10c30
Amanda Gxagxa <amandagxagxa@gmail.com>
2018-05-28 09:46:49 +0200

    I am commiting the changes I have made

--- test/registration_numbers.test.js
+++ test/registration_numbers.test.js
@@ -9,7 +9,7 @@ describe('Returns Cape Town because startswith CA', function() {
 
   });
 
-  it("should not add invalid reg numbers", function() {
+  it("should not add invalid registration numbers", function() {
     var registration = Registration();
     registration.addRegNumber('CC 124567');
     assert.notEqual(registration.getRegNum(), {'CC 124567':0});
@@ -26,12 +26,7 @@ it("should be able to add the registration number that start with CK.", function
 
     assert.deepEqual(registration.getRegMapKeys(), {'CA 124567':0});
   });
-  it("should be able to take all from Cape Town.", function() {
-    var registration = Registration();
-    registration.addRegNumber('CA 124567')
-    registration.addRegNumber('CA 124568')
-    assert.equal(registration.findFrom('Cape Town'), "2");
-  });
+
   it("should be able to print true when added allowed registration number.", function() {
     var registration = Registration();
     assert.equal(registration.addRegNumber('CA 124567'), true);
@@ -40,10 +35,19 @@ it("should be able to add the registration number that start with CK.", function
 });
 
 describe('Filter Function', function(){
-  it('Should filter reg by CA', function(){
+  it('Should take all egistration numbers added', function(){
     var factory = Registration();
-    var regMap = {'CA 12345':0};
+    factory.addRegNumber('CK 124567')
+    factory.addRegNumber('CA 124567')
+    factory.addRegNumber('CL 124568')
 
-    assert.equal(factory.findFrom(regMap), {'CA 123345':0})
+    assert.deepEqual(factory.findFrom('All'), ['CK 124567','CA 124567','CL 124568'])
+  });
+  it("should be able to take only registrations from Cape Town.", function() {
+    var registration = Registration();
+    registration.addRegNumber('CJ 124567')
+    registration.addRegNumber('CA 124567')
+    registration.addRegNumber('CA 124568')
+    assert.deepEqual(registration.findFrom('CA'), ['CA 124567', 'CA 124568']);
   });
 });
